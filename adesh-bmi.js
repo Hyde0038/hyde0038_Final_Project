10f8cb74b9dbfb8748050f58f243112b2bf04d7f
hyde0038 <hyde0038@algonquinlive.com>
2024-04-11 11:58:34 -0400

    Fixed

--- adesh-bmi.js
+++ adesh-bmi.js
@@ -1,7 +1,6 @@
 
 function calculateBMI(weight = 90, height = 1.91) {
     let bmi = ((weight / height) / height);
-
     interpretBMI(bmi);
 }
 
@@ -9,7 +8,7 @@ function interpretBMI(bmi){
     console.log(bmi.toFixed(2));
     if( bmi < 18.5 ) {
         console.log("Underweight");
-    } else if ( bmi < 18.5 && bmi > 25 ) {
+    } else if ( bmi >= 18.5 && bmi < 25 ) {
         console.log("Normal weight");
     } else if ( bmi >= 25 && bmi < 30 ) {
         console.log("Overweight");
